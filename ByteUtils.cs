ba5ee4739ba99ca275f66d7049b4a205b5a39e16
sangeren <ch199108@gmail.com>
2019-09-04 14:53:13 +0800

    init code

---
+++ ByteUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Duration.Mine.Mp4
+{
+    internal sealed class ByteUtils
+    {
+        private ByteUtils()
+        {
+        }
+
+        public static int IndexOf(byte[] array, byte[] pattern, int offset)
+        {
+            int success = 0;
+            for (int i = offset; i < array.Length; i++)
+            {
+                if (array[i] == pattern[success])
+                {
+                    success++;
+                }
+                else
+                {
+                    success = 0;
+                }
+
+                if (pattern.Length == success)
+                {
+                    return i - pattern.Length + 1;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ Mp4Duration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Duration.Mine.Mp4
+{
+    /// <summary>
+    /// mp4 format
+    /// </summary>
+    public class Mp4Duration
+    {
+        /// <summary>
+        /// get mp4 duration
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns>second</returns>
+        public static double GetMp4Duration(string fileName)
+        {
+            ulong timeScale = 0;
+            ulong timeUnits = 0;
+            int byteSize = 4;
+
+            string str = "mvhd";
+            var charStr = Encoding.ASCII.GetBytes(str);
+
+            byte[] array = File.ReadAllBytes(fileName);
+            var indexOfCuurent = ByteUtils.IndexOf(array, charStr, 0);
+            indexOfCuurent += 4;
+
+            if (array[indexOfCuurent] == 1)
+            {
+                byteSize = 8;
+            }
+            indexOfCuurent += 4;//now point to the createa date
+
+            //byte[] createDateByte = new byte[byteSize];
+            //Array.Copy(array, indexOfCuurent, createDateByte, 0, byteSize);
+            //var createDate = BitConverter.ToUInt32(createDateByte, 0);
+            indexOfCuurent += byteSize;
+            indexOfCuurent += byteSize;
+
+            byte[] timeScaleByte = new byte[4];
+            Array.Copy(array, indexOfCuurent, timeScaleByte, 0, 4);
+            timeScale = BitConverter.ToUInt32(timeScaleByte, 0);
+
+            indexOfCuurent += 4;
+            byte[] timeUnitsByte = new byte[4];
+            Array.Copy(array, indexOfCuurent, timeUnitsByte, 0, 4);
+            timeUnits = BitConverter.ToUInt32(timeUnitsByte, 0);
+
+            timeScale = InfoFlip(timeScale);
+            timeUnits = InfoFlip(timeUnits);
+
+            return timeUnits * 1.0 / timeScale;
+        }
+
+        static UInt64 InfoFlip(UInt64 vla)
+        {
+            UInt64 tem = 0;
+            tem += (vla & 0x000000FF) << 24;
+            tem += (vla & 0xFF000000) >> 24;
+            tem += (vla & 0x0000FF00) << 8;
+            tem += (vla & 0x00FF0000) >> 8;
+            return tem;
+        }
+    }
+}
